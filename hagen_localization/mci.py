05a3dedd246cda61f850e968b9b0fd2a4e269991
Ahmed Ali <ahmedmohsen022@gmail.com>
2022-11-06 14:21:05 +0300

    solve example1 in mci.py

--- hagen_localization/mci.py
+++ hagen_localization/mci.py
@@ -3,22 +3,44 @@ import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
 
-# Example 01 
-a = 0
-b = 5
-x = np.random.uniform(low=a, high=b, size=300)
-# Monte Carlo integration
-y = x**2
+# # Example 01 (General case)
+# a = 0
+# b = 1
+# x = np.random.uniform(low=a, high=b, size=100)
 
-y_bar = (b-a)/ x.shape[0] * (x**2).sum()
+# def integrand(x):
+#     return  4*np.sqrt(1-x**2)
 
+# # Monte Carlo integration
+# y = integrand(x)
+# y_bar = (b-a)/ x.shape[0] * y.sum()
+
+# #Actual Integration 
+# I = quad(integrand, a, b,)
+# print("Actual integration: ", I[0], "Monte Carlo integration: ", y_bar)
+# plt.scatter(x, y)
+# plt.show()
+
+
+
+
+# Example 01 (find the value of pi) 
+r = 1
+
+a = -r
+b = r
+x = np.random.uniform(low=a, high=b, size=100)
+
+# area of circle is integration of half circle multiplied by two 
+# it can be written also as integration of quarter of a circle multiplied by four as in lecture  (limits will be from 0 to r)
 def integrand(x):
-    return  x**2
+    return  2*np.sqrt(b-x**2)
 
 # Monte Carlo integration
 y = integrand(x)
-y_bar = 
-    
+y_bar = (b-a)/ x.shape[0] * y.sum()
+
+#Actual Integration 
 I = quad(integrand, a, b,)
 print("Actual integration: ", I[0], "Monte Carlo integration: ", y_bar)
 plt.scatter(x, y)
