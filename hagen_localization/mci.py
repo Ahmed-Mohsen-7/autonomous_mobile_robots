17a157bc94f6d1fad39681723b95b64a2f11b870
Ahmed Ali <ahmedmohsen022@gmail.com>
2022-11-09 21:14:19 +0300

    change mci

--- hagen_localization/mci.py
+++ hagen_localization/mci.py
@@ -1,27 +1,6 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from scipy.integrate import quad
-
-# # Example 01 
-# a = 0
-# b = 5
-# x = np.random.uniform(low=a, high=b, size=100)
- 
-# def integrand(x):
-#     return  x**2
-
-# # Monte Carlo integration
-# y = integrand(x)
-# y_bar = ((b-a)/x.shape[0])*np.sum(y)
-    
-# I = quad(integrand, a, b,)
-# print("Actual integration: ", I[0], "Monte Carlo integration: ", y_bar)
-# plt.scatter(x, y)
-# plt.show()
-
-
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
 n=1e5
 radius = 5
