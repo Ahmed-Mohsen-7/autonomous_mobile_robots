83a906a661c0864dbbe91481c8dd885adc82468f
Ahmed Ali <ahmedmohsen022@gmail.com>
2022-11-25 17:19:41 +0300

    add solution to ass4

---
+++ Assignments_solutions/Ass_3.py
@@ -0,0 +1,220 @@
+import sympy
+sympy.init_printing(use_latex='mathjax')
+from sympy import symbols, Matrix
+from IPython.display import display
+from numpy import array, sqrt
+import numpy as np 
+from numpy.random import randn
+from math import sqrt, tan, cos, sin, atan2
+import matplotlib.pyplot as plt
+from math import sqrt
+from copy import deepcopy
+from matplotlib.patches import Ellipse
+import math
+
+class EKFLocalization():
+    def __init__(self,dt, std_vel, std_steer, dim_x=3, dim_z=2, dim_u=2):
+        self.dt = dt
+        self.std_vel = std_vel
+        self.std_steer = std_steer
+        self.get_linearized_motion_model()
+        self.subs = {self._x: 0, self._y: 0, self._vt:0, self._wt:0, self._dt:dt, self._theta:0}
+        
+        self.x = np.zeros((dim_x, 1)) # state
+        self.P = np.eye(dim_x)        # uncertainty covariance
+        self.R = np.eye(dim_z)        # state uncertainty
+        self.Q = np.eye(dim_x)        # process uncertainty
+        self.y = np.zeros((dim_z, 1)) # residual
+        self.z =  np.array([None]*dim_z)
+        self.K = np.zeros(self.x.shape) # kalman gain
+        self.y = np.zeros((dim_z, 1))
+        self._I = np.eye(dim_x)
+        self.ctrl_state = 0
+        self.dmin = 0.5   #goal tolerance
+
+    def get_linearized_motion_model(self):
+        x, y, theta, vt, wt, dt = sympy.symbols('x, y, theta, v_t, omega_t, delta_t')
+        f = sympy.Matrix([[x+-(vt/wt)*sympy.sin(theta)+ (vt/wt)*sympy.sin(theta+ wt*dt)]
+                  ,[y+ (vt/wt)*sympy.cos(theta) - (vt/wt)*sympy.cos(theta+ wt*dt)]
+                  , [theta+wt*dt]])
+        f = sympy.Matrix([[x+-(vt/wt)*sympy.sin(theta)+ (vt/wt)*sympy.sin(theta+ wt*dt)]
+                  ,[y+ (vt/wt)*sympy.cos(theta) - (vt/wt)*sympy.cos(theta+ wt*dt)]
+                  , [theta+wt*dt]])
+
+        self._x, self._y, self._theta, self._vt, self._wt, self._dt = x, y, theta, vt, wt, dt
+        self.state = sympy.Matrix([x, y, theta])
+        self.control = sympy.Matrix([vt, wt])
+        self.F_j = f.jacobian(self.state)
+        self.V_j = f.jacobian(self.control)
+        return
+    
+    def x_forward(self, x, u, dt):
+        r = u[0]/u[1]
+        theta = x[2]
+        rotation = x[2] + u[1]*dt 
+        x_plus = np.array([x[0] + -r*sin(theta) + r*sin(rotation),
+                       x[1] + r*cos(theta) - r*cos(rotation),
+                       x[2] + u[1]*dt])
+        return  x_plus 
+        
+    def get_linearized_measurement_model(self, x, landmark_pos):
+        px = landmark_pos[0]
+        py = landmark_pos[1]
+        hyp = (px - x[0, 0])**2 + (py - x[1, 0])**2
+        dist = sqrt(hyp)
+        Hx = array([[dist],[atan2(py - x[1, 0], px - x[0, 0]) - x[2, 0]]])
+        H = array([[-(px - x[0, 0]) / dist, -(py - x[1, 0]) / dist, 0]
+                , [ (py - x[1, 0]) / hyp,  -(px - x[0, 0]) / hyp, -1]])
+        return Hx, H
+        
+    def control_strategy(self,x,x_goal):
+        r =  np.sqrt(x_goal[0]**2 + x_goal[1]**2) / 4
+        done = False
+        #move along the first circle
+        if (self.ctrl_state == 0 ):
+                self.D = np.sqrt((x[0]-x_goal[0]/2)**2 
+                                        + (x[1]-x_goal[1]/2)**2)
+
+                if(self.D < self.dmin):
+                    print("Reach to the intermediate point")
+                    self.ctrl_state  = 1
+                
+                v = 1
+                w = -1 / r
+                #print("Distance to the goal 1: ", self.D)
+        #move along the second circle
+        else:      
+            self.D = np.sqrt((x[0]-x_goal[0])**2 
+                                        + (x[1]-x_goal[1])**2)    
+            if(self.D < self.dmin):
+                print("Reach to the goal")
+                done = True
+            v = 1
+            w = 1 / r
+        return v,w,done
+
+
+
+    def residual(self, a, b):
+        """ compute residual (a-b) between measurements containing 
+        [range, bearing]. Bearing is normalized to [-pi, pi)"""
+        y = a - b
+        y[1] = y[1] % (2 * np.pi)    # force in range [0, 2 pi)
+        if y[1] > np.pi:             # to [-pi, pi)
+            y[1] -= 2 * np.pi
+        return y
+    
+    def predict(self, u):
+        self.x = self.x_forward(self.x, u, self.dt)
+        self.subs[self._x] = self.x[0, 0]
+        self.subs[self._y] = self.x[1, 0]
+        self.subs[self._theta] = self.x[2, 0]
+        self.subs[self._vt] = u[0]
+        self.subs[self._wt] = u[1]
+
+        F = array(self.F_j.evalf(subs=self.subs)).astype(float)
+        V = array(self.V_j.evalf(subs=self.subs)).astype(float)
+
+        # covariance in the control space
+        M = array([[self.std_vel**2, 0],  [0, self.std_steer**2]])
+
+        self.P = F @ self.P @ F.T + V @ M @ V.T + self.Q
+
+    def ekf_update(self, z, landmarks):
+        Hx, H = self.get_linearized_measurement_model(self.x, landmarks)
+        PHT = np.dot(self.P, H.T)
+        self.K = PHT.dot(np.linalg.inv(np.dot(H, PHT) + self.R))
+        self.y = self.residual(z, Hx)
+        self.x = self.x + np.dot(self.K, self.y)
+
+        # P = (I-KH)P(I-KH)' + KRK' is more numerically stable
+        # P = (I-KH)P is the optimal gain 
+        I_KH = self._I - np.dot(self.K, H)
+        self.P = np.dot(I_KH, self.P).dot(I_KH.T) + np.dot(self.K, self.R).dot(self.K.T)
+        self.z = deepcopy(z)
+        
+    def z_landmark(self, lmark, sim_pos, std_rng, std_brg):
+        x, y = sim_pos[0, 0], sim_pos[1, 0]
+        d = np.sqrt((lmark[0] - x)**2 + (lmark[1] - y)**2)  
+        a = atan2(lmark[1] - y, lmark[0] - x) - sim_pos[2, 0]
+        z = np.array([[d + randn()*std_rng],
+                    [a + randn()*std_brg]])
+        return z
+    
+    def covariance_ellipse(self, P, deviations=1):
+        U, s, _ = np.linalg.svd(P)
+        orientation = math.atan2(U[1, 0], U[0, 0])
+        width = deviations * math.sqrt(s[0])
+        height = deviations * math.sqrt(s[1])
+        if height > width:
+            raise ValueError('width must be greater than height')
+        return (orientation, width, height)
+    
+    
+    def plot_covariance_ellipse(self, mean, cov, std=None, facecolor='b', edgecolor='g', alpha=0.7,ls='solid'):
+        ellipse = self.covariance_ellipse(cov)
+        ax = plt.gca()
+        angle = np.degrees(ellipse[0])
+        width = ellipse[1] * 2.
+        height = ellipse[2] * 2.
+        e = Ellipse(xy=mean, width=std*width, height=std*height, angle=angle, facecolor=facecolor, edgecolor=edgecolor, alpha=alpha, lw=2, ls=ls)
+        ax.add_patch(e)
+        x, y = mean
+        plt.scatter(x, y, marker='+', color=edgecolor)
+        a = ellipse[0]
+        h, w = height/4, width/4
+        plt.plot([x, x+ h*cos(a+np.pi/2)], [y, y + h*sin(a+np.pi/2)])
+        plt.plot([x, x+ w*cos(a)], [y, y + w*sin(a)])
+    
+    def run_localization(self, landmarks, std_range, std_bearing, step=5, ellipse_step=2000, ylim=None, iteration_num=5):
+        self.x = array([[0, 0, np.pi/4]]).T # x, y, steer angle
+        self.P = np.diag([.1, .1, .1])
+        self.R = np.diag([std_range**2, std_bearing**2])
+        sim_pos = self.x.copy()
+        u = array([0.001, 0.001]) 
+        plt.figure()
+        plt.scatter(landmarks[:, 0], landmarks[:, 1], marker='s', s=60,label="landmarks")
+        track1 = []
+        track2 = []
+        for i in range(iteration_num):
+            sim_pos = self.x_forward(sim_pos, u, dt) # simulate robot
+            v,w,done = self.control_strategy(sim_pos, x_goal)  #get control signals based on simulated robot
+            u[0],u[1] = v,w
+            #reach the goal
+            if done:
+                break
+            track1.append(sim_pos)
+            if i % step == 0:
+                self.predict(u=u)
+                #if i % ellipse_step == 0:
+                    #self.plot_covariance_ellipse((self.x[0,0], self.x[1,0]), self.P[0:2, 0:2], std=6, facecolor='k', alpha=0.3)
+                     
+                for lmark in landmarks:
+                    z = self.z_landmark(lmark, sim_pos, std_range, std_bearing)
+                    self.ekf_update(z, lmark)
+                #if i % ellipse_step == 0:
+                    #self.plot_covariance_ellipse((self.x[0,0], self.x[1,0]), self.P[0:2, 0:2], std=6, facecolor='g', alpha=0.8)
+                track2.append(self.x)
+        track1 = np.array(track1)
+        track2 = np.array(track2)
+        plt.plot(track1[:, 0], track1[:,1], color='k', lw=2,label = "Desired Path")
+        plt.plot(track2[:, 0], track2[:,1], color='g', lw=2,label = "EKF")
+        plt.plot(0,0,'oy',label="Start")
+        plt.plot(D /2,S/2,'ob',label="Intermediate")
+        plt.plot(D ,S,'or',label="Goal")
+        plt.axis('equal')
+        plt.legend()
+        plt.title("Robot EKF Localization")
+        if ylim is not None: plt.ylim(*ylim)
+        plt.show()
+        return ekf
+        
+dt = 0.1
+#Assumed parameters
+D = 10
+S = -14.14
+x_goal = np.array([[D],[S]])
+
+landmarks = array([[5, 30], [5, -30], [-5, 0]])
+ekf = EKFLocalization(dt, std_vel=4, std_steer=np.radians(1))
+ekf.run_localization(landmarks, std_range=0.3, std_bearing=0.1, ellipse_step=1000, iteration_num=600)
